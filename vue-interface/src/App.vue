5f1111c2c3fb87a8cd16a06619eea67aaa0c15cf
codingChewie <codingChewie@gmail.com>
2020-10-23 12:57:02 -0500

    adding a bootstrap template

--- vue-interface/src/App.vue
+++ vue-interface/src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <div id="main-app" class="container">
-    <appointment-list :appointments="appointments" />
+    <div class="row justify-content-center">
+      <appointment-list :appointments="appointments" />
+    </div>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import AppointmentList from "./components/AppointmentList";
 import axios from "axios";
 
 export default {
   name: "MainApp",
+  components: {
+    AppointmentList,
+  },
   data: function() {
     return {
       title: "Appointment List",
       appointments: [],
     };
   },
-  components: {
-    FontAwesomeIcon,
-    AppointmentList,
-  },
+
   mounted() {
     axios
       .get("./data/appointments.json")
